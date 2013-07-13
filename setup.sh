ecd0b30e7bd172132dc9d58a55651612f0282ccf
Christopher Duncombe Rae <christopher.duncombe.rae@gmail.com>
2013-07-13 12:22:29 +0000

    Making the hidden files visible.

--- setup.sh
+++ setup.sh
@@ -34,10 +34,11 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+git clone https://github.com/duncombe/dotfiles.git
+# link dotfiles to dotted files in the home directory
+ln -sb dotfiles/screenrc .screenrc
+ln -sb dotfiles/bash_profile .bash_profile
+ln -sb dotfiles/bashrc .bashrc
+ln -sb dotfiles/bashrc_custom .bashrc_custom
+ln -sf dotfiles/emacs.d .emacs.d
 
